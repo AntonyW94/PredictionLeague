eed7d5f0adf00263be7aa1e4437f08dd15e9d5c3
Antony Willson <antony.willson@evolution-internet.com>
2025-07-09 11:53:06 +0100

    Fix PredictionSubmission Range

--- PredictionLeague.Shared/Predictions/PredictionSubmission.cs
+++ PredictionLeague.Shared/Predictions/PredictionSubmission.cs
@@ -7,9 +7,9 @@ public class PredictionSubmission
     [Required]
     public int MatchId { get; init; }
 
-    [Range(0, 99)]
+    [Range(0, 9)]
     public int PredictedHomeScore { get; init; }
 
-    [Range(0, 99)]
+    [Range(0, 9)]
     public int PredictedAwayScore { get; init; }
 }
