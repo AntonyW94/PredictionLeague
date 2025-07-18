76645791f25eb8937691f03cf2e0e9d76ba1abda
Antony Willson <antony.willson@evolution-internet.com>
2025-07-18 20:37:46 +0100

    Refactor Team Requests and Validation

---
+++ PredictionLeague.Validators/Admin/Teams/BaseTeamRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using PredictionLeague.Contracts.Admin.Teams;
+
+namespace PredictionLeague.Validators.Admin.Teams;
+
+public abstract class BaseTeamRequestValidator<T> : AbstractValidator<T> where T : BaseTeamRequest
+{
+    protected BaseTeamRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Please enter a team name.")
+            .Length(2, 100).WithMessage("The team name must be between 2 and 100 characters.");
+
+        RuleFor(x => x.LogoUrl)
+            .NotEmpty().WithMessage("Please provide a URL for the team's logo.")
+            .Must(BeAValidUrl).WithMessage("A valid logo URL is required.");
+    }
+
+    private bool BeAValidUrl(string url)
+    {
+        return Uri.TryCreate(url, UriKind.Absolute, out _);
+    }
+}
--- PredictionLeague.Validators/Admin/Teams/UpdateTeamRequestValidator.cs
+++ PredictionLeague.Validators/Admin/Teams/UpdateTeamRequestValidator.cs
@@ -1,25 +1,7 @@
-﻿using FluentValidation;
-using PredictionLeague.Contracts.Admin.Teams;
+﻿using PredictionLeague.Contracts.Admin.Teams;
 using System.Diagnostics.CodeAnalysis;
 
 namespace PredictionLeague.Validators.Admin.Teams;
 
 [SuppressMessage("ReSharper", "UnusedType.Global")]
-public class UpdateTeamRequestValidator : AbstractValidator<UpdateTeamRequest>
-{
-    public UpdateTeamRequestValidator()
-    {
-        RuleFor(x => x.Name)
-            .NotEmpty().WithMessage("Please enter a team name.")
-            .Length(2, 100).WithMessage("The team name must be between 2 and 100 characters.");
-
-        RuleFor(x => x.LogoUrl)
-            .NotEmpty().WithMessage("Please provide a URL for the team's logo.")
-            .Must(BeAValidUrl).WithMessage("A valid logo URL is required.");
-    }
-
-    private bool BeAValidUrl(string url)
-    {
-        return Uri.TryCreate(url, UriKind.Absolute, out _);
-    }
-}
+public class UpdateTeamRequestValidator : BaseTeamRequestValidator<UpdateTeamRequest>;
