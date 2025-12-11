e3bda14f3e2bc10bb1fb240cb1f798af0f384b10
Antony Willson <antony.willson@evolution-internet.com>
2025-12-11 15:04:15 +0000

    Boosts Remaining Shown in Badge

--- PredictionLeague.Contracts/Predictions/PredictionLeagueDto.cs
+++ PredictionLeague.Contracts/Predictions/PredictionLeagueDto.cs
@@ -4,5 +4,6 @@ public class PredictionLeagueDto
 {
     public int LeagueId { get; init; }
     public string Name { get; init; } = string.Empty;
-    public bool HasBoosts { get; init; } = false;
+    public bool HasBoosts { get; init; }
+    public string? SelectedBoostCode { get; init; }
 }
