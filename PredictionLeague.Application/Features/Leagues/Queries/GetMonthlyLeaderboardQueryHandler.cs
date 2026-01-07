7e327a6b33754ded85ead92a9dbfa7ebd4637a3e
Antony Willson <antony.willson@evolution-internet.com>
2026-01-07 16:39:53 +0000

    Add Rank Arrows to Monlthy Leaderboard

--- PredictionLeague.Application/Features/Leagues/Queries/GetMonthlyLeaderboardQueryHandler.cs
+++ PredictionLeague.Application/Features/Leagues/Queries/GetMonthlyLeaderboardQueryHandler.cs
@@ -31,20 +31,40 @@ public class GetMonthlyLeaderboardQueryHandler : IRequestHandler<GetMonthlyLeade
                 RANK() OVER (ORDER BY COALESCE(SUM(lrr.[BoostedPoints]), 0) DESC) AS [Rank],
                 u.[FirstName] + ' ' + LEFT(u.[LastName], 1) AS PlayerName,
                 COALESCE(SUM(lrr.[BoostedPoints]), 0) AS [TotalPoints],
-                u.[Id] AS [UserId]
+                u.[Id] AS [UserId],
+
+                CASE 
+                    WHEN EXISTS (
+                        SELECT 1 
+                        FROM [Rounds] r
+                        WHERE r.[Id] IN (SELECT [Id] FROM [MonthlyRounds])
+                        AND r.[Status] = @InProgressStatus                
+                    ) THEN stats.[SnapshotMonthRank] 
+                    ELSE NULL 
+                END AS [SnapshotRank],
+
+                CASE WHEN EXISTS (
+                    SELECT 1 
+                    FROM [Rounds] r 
+                    JOIN [Leagues] l ON r.[SeasonId] = l.[SeasonId] 
+                    WHERE l.[Id] = @LeagueId AND r.[Status] = @InProgressStatus
+                ) THEN 1 ELSE 0 END AS [IsRoundInProgress]
             FROM 
                 [LeagueMembers] lm
             JOIN 
                 [AspNetUsers] u ON lm.[UserId] = u.[Id]
             LEFT JOIN 
 	            [LeagueRoundResults] lrr ON lm.[UserId] = lrr.[UserId] AND lrr.[LeagueId] = @LeagueId AND lrr.[RoundId] IN (SELECT [Id] FROM [MonthlyRounds])
+            LEFT JOIN 
+                [LeagueMemberStats] stats ON lm.[LeagueId] = stats.[LeagueId] AND lm.[UserId] = stats.[UserId]
             WHERE 
                 lm.[LeagueId] = @LeagueId
                 AND lm.[Status] = @ApprovedStatus
             GROUP BY
                 u.[FirstName],
                 u.[LastName],
-                u.[Id]
+                u.[Id],
+                stats.[SnapshotMonthRank]
             ORDER BY
                 [Rank] ASC,
                 [PlayerName] ASC;";
@@ -56,7 +76,8 @@ public class GetMonthlyLeaderboardQueryHandler : IRequestHandler<GetMonthlyLeade
             {
                 request.LeagueId,
                 request.Month,
-                ApprovedStatus = nameof(LeagueMemberStatus.Approved)
+                ApprovedStatus = nameof(LeagueMemberStatus.Approved),
+                InProgressStatus = nameof(RoundStatus.InProgress),
             }
         );
     }
